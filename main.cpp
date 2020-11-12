412dfb5a06b4021aa40f39cbfddbfee1a2e4d307
Konstantin-Lis <55928181+Konstantin-Lis@users.noreply.github.com>
2020-11-12 19:18:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
-#include "Sorts.h"
+#include "Interface.h"
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-//TODO: Rewrite Counting_sort
-//TODO: Write Quick_sort
-
 int main() {
+    Interface<int>();
+
+    return 0;
+
+    /*
     int n;
     cin >> n;
     Sequence<int>* vec = new ArraySequence<int>(n);
@@ -16,10 +18,11 @@ int main() {
         vec->Set(i, a);
     }
 //    vector<string> vecs = {"aaab", "aaba", "abaa", "bbaa", "aabb", "baab"};
-    PrintVec(vec);
+//    PrintVec(vec);
+//    PrintVec(Bubble_Sort(vec));
 //    PrintVec(Merge_Sort(vec));
 //    PrintVec(Insertion_Sort(vec));
-    PrintVec(Counting_Sort(vec));
+//    PrintVec(Counting_Sort(vec));
 //    PrintVec(Choise_Sort(vec));
 
 
@@ -27,5 +30,6 @@ int main() {
 //    Learn_Sorting_Time(Merge_Sort, vec, "Merge sort");
 //    Learn_Sorting_Time(Choise_Sort, vec, "Choise sort");
 //    Learn_Sorting_Time(Insertion_Sort, vec, "Insertion sort");
-    return 0;
+//    Learn_Sorting_Time(Bubble_Sort, vec, "Bubble sort");
+     */
 }
